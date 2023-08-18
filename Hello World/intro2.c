284af3b10b5737cad23033b59dcb83c9059455ae
Moutik Patel <85564675+moutik7@users.noreply.github.com>
2023-08-18 17:04:04 -0400

    Hello World Folder Uploaded to C rep

---
+++ Hello World/intro2.c
@@ -0,0 +1,14 @@
+
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("+-------------------------------+\n");
+	printf("| Introduction to C Programming |\n");
+	printf("+-------------------------------+\n");
+	printf("Program written by:\n");
+	printf("Moutik Patel");
+
+	return 0;
+}
